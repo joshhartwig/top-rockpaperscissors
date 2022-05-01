2e8bb7eef003c556cede93af0629d9a8eebc672c
Josh Hartwig <joshman211@gmail.com>
2022-04-30 22:29:00 -0500

    completed computer play function and playround. Fix beginning of script.

--- scripts.js
+++ scripts.js
@@ -9,47 +9,54 @@ paper beats rock, rock beats scissors, scissors beat paper or tie if both select
 
 */
 
+console.log('let\'s play a game of rock paper scissors');
+playRound(parseInt(prompt('enter a selection r=1,p=2,s=3')), computerSelection());
 
-function playRound(playerSelection, computerSelection){
-    
+function computerPlay() {
+    return Math.floor(Math.random() * 3) + 1;
+}
+
+
+function playRound(playerSelection, computerSelection) {
+
     //tie
-    if(playerSelection === computerSelection) {
+    if (playerSelection === computerSelection) {
         console.log('tie');
         return;
     }
-    
+
     //rock vs paper
-    if(playerSelection === 1 && computerSelection == 2) {
+    if (playerSelection === 1 && computerSelection == 2) {
         console.log(`Computer wins Paper beats Rock. -- Player Selection: ${playerSelection} Computer Selection: ${computerSelection}`);
         return;
     }
 
     //rock vs scissors
-    if(playerSelection === 1 && computerSelection == 3) {
+    if (playerSelection === 1 && computerSelection == 3) {
         console.log(`Player wins, Rock beats Scissors. -- Player Selection: ${playerSelection} Computer Selection: ${computerSelection}`);
         return;
     }
 
     //paper vs rock
-    if(playerSelection === 2 && computerSelection == 1) {
+    if (playerSelection === 2 && computerSelection == 1) {
         console.log(`Player wins, Rock beats Paper. -- Player Selection: ${playerSelection} Computer Selection: ${computerSelection}`);
         return;
     }
 
     //paper vs scissors
-    if(playerSelection === 2 && computerSelection == 3) {
+    if (playerSelection === 2 && computerSelection == 3) {
         console.log(`Computer wins, Scissors beats Paper. -- Player Selection: ${playerSelection} Computer Selection: ${computerSelection}`);
         return;
     }
 
     //scissors vs rock
-    if(playerSelection === 3 && computerSelection == 1) {
+    if (playerSelection === 3 && computerSelection == 1) {
         console.log(`Computer wins, Rock beats Scissors. -- Player Selection: ${playerSelection} Computer Selection: ${computerSelection}`);
         return;
     }
 
     //scissors vs rock
-    if(playerSelection === 3 && computerSelection == 2) {
+    if (playerSelection === 3 && computerSelection == 2) {
         console.log(`Player wins, Scissors beat paper. -- Player Selection: ${playerSelection} Computer Selection: ${computerSelection}`);
         return;
     }
