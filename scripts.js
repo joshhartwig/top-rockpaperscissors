32727c4739fb3c87249dfbcc85bc1305daf9cd22
Josh Hartwig <joshman211@gmail.com>
2022-05-01 17:17:58 -0500

    added computer playing function and util functions to map a string to a numerical choice.

--- scripts.js
+++ scripts.js
@@ -9,13 +9,35 @@ paper beats rock, rock beats scissors, scissors beat paper or tie if both select
 
 */
 
-console.log('let\'s play a game of rock paper scissors');
-playRound(parseInt(prompt('enter a selection r=1,p=2,s=3')), computerSelection());
+let playerScore = 0;
+let computerScore = 0;
 
+console.log('let\'s play a game of rock paper scissors');
+
+for(let i = 0; i < 100; i++){
+    let guess = Math.floor(Math.random() * 3) + 1;
+    playRound(guess, computerPlay());
+}
+
+console.log(`Game Over: Player Score is: ${playerScore} Computer Score is: ${computerScore}. X wins.`); 
+
+//pick random number between 1 and 3
 function computerPlay() {
     return Math.floor(Math.random() * 3) + 1;
 }
 
+function mapStringToNumber(choice){
+    switch (choice.toLowerCase()) {
+        case 'rock':
+            return 1;
+        case 'paper':
+            return 2;
+        case 'scissors':
+            return 3;
+        default:
+            break;
+    }
+}
 
 function playRound(playerSelection, computerSelection) {
 
@@ -28,36 +50,42 @@ function playRound(playerSelection, computerSelection) {
     //rock vs paper
     if (playerSelection === 1 && computerSelection == 2) {
         console.log(`Computer wins Paper beats Rock. -- Player Selection: ${playerSelection} Computer Selection: ${computerSelection}`);
+        computerScore++;
         return;
     }
 
     //rock vs scissors
     if (playerSelection === 1 && computerSelection == 3) {
         console.log(`Player wins, Rock beats Scissors. -- Player Selection: ${playerSelection} Computer Selection: ${computerSelection}`);
+        playerScore++;
         return;
     }
 
     //paper vs rock
     if (playerSelection === 2 && computerSelection == 1) {
         console.log(`Player wins, Rock beats Paper. -- Player Selection: ${playerSelection} Computer Selection: ${computerSelection}`);
+        playerScore++;
         return;
     }
 
     //paper vs scissors
     if (playerSelection === 2 && computerSelection == 3) {
         console.log(`Computer wins, Scissors beats Paper. -- Player Selection: ${playerSelection} Computer Selection: ${computerSelection}`);
+        computerScore++;
         return;
     }
 
     //scissors vs rock
     if (playerSelection === 3 && computerSelection == 1) {
         console.log(`Computer wins, Rock beats Scissors. -- Player Selection: ${playerSelection} Computer Selection: ${computerSelection}`);
+        computerScore++;
         return;
     }
 
     //scissors vs rock
     if (playerSelection === 3 && computerSelection == 2) {
         console.log(`Player wins, Scissors beat paper. -- Player Selection: ${playerSelection} Computer Selection: ${computerSelection}`);
+        playerScore++;
         return;
     }
 
